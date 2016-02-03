68fa8e97e4c9fd4c035d23c47b19ed92b6452eb9
Askhat Sakhabiev <jojbot@gmail.com>
2016-02-03 13:12:44 +0300

    add Point, Vector, Line

--- main.cpp
+++ main.cpp
@@ -1,8 +1,101 @@
 #include <iostream>
+#include <cmath>
+#include <vector>
 
 using namespace std;
 
+const double EPS = 1e-5;
+
+struct Point
+{
+    double x, y;
+    Point(double _x, double _y)
+    {
+       x = _x;
+       y = _y;
+    }
+};
+
+struct Vector
+{
+    double x, y;
+    Vector(double _x, double _y)
+    {
+        x = _x;
+        y = _y;
+    }
+};
+
+istream& operator >> (istream& _in, Point& a)
+{
+   _in >> a.x >> a.y;
+   return _in;
+}
+
+ostream& operator << (ostream& _out, Point a)
+{
+    _out << a.x << " " << a.y;
+    return _out;
+}
+
+istream& operator >> (istream& _in, Vector& a)
+{
+   _in >> a.x >> a.y;
+   return _in;
+}
+
+ostream& operator << (ostream& _out, Vector a)
+{
+    _out << a.x << " " << a.y;
+    return _out;
+}
+
+Vector operator - (Point a, Point b)
+{
+    return Vector(a.x - b.x, a.y - b.y);
+}
+
+Vector operator + (Vector a, Vector b)
+{
+    return Vector(a.x + b.x, a.y + b.y);
+}
+
+Point operator + (Point a, Vector b)
+{
+    return Point(a.x + b.x, a.y + b.y);
+}
+
+bool Equal(double a, double b)
+{
+    return abs(a - b) < EPS;
+}
+
+bool operator == (Point a, Point b)
+{
+    return Equal(a.x, b.x) and Equal(a.y, b.y);
+}
+
+bool operator == (Vector a, Vector b)
+{
+    return Equal(a.x, b.x) and Equal(a.y, b.y);
+}
+
+struct Line
+{
+    double a, b, c;
+    Line(Point x, Point y)
+    {
+        a = y.y - x.y;
+        b = x.x - y.x;
+        c = -a * x.x - b * x.y;
+    }
+    Line(Point x, Vector y)
+    {
+        Line(x, x + y);
+    }
+};
+
+
 int main()
 {
-    cout << "HLW" << endl;
 }
