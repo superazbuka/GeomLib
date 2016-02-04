fb4773cbf846f9f5e2ebce08c7ef3791c907a5ff
Askhat Sakhabiev <jojbot@gmail.com>
2016-02-04 17:44:00 +0300

    add Ray, Segments and some functions and operators

--- main.cpp
+++ main.cpp
@@ -67,6 +67,11 @@ double Len(Vector a)
     return sqrt(a.x * a.x + a.y * a.y);
 }
 
+double Dist(Point a, Point b)
+{
+    return Len(a - b);
+}
+
 Vector operator + (Vector a, Vector b)
 {
     return Vector(a.x + b.x, a.y + b.y);
@@ -102,6 +107,11 @@ Vector operator / (Vector a, double b)
     return a * (1 / b);
 }
 
+Vector operator * (double a, Vector b)
+{
+    return b * a;
+}
+
 double operator * (Vector a, Vector b)
 {
     return a.x * b.x * a.y * b.y;
@@ -111,7 +121,6 @@ double operator ^ (Vector a, Vector b)
 {
     return a.x * b.y - a.y * b.x;
 }
-
 
 struct Line
 {
@@ -199,6 +208,11 @@ double Dist(Line a, Point b)
     return a[b];
 }
 
+double Dist(Point a, Line b)
+{
+    return Dist(b, a);
+}
+
 enum TypeOfIntersect {EMPTY, POINT, INF}; 
 
 TypeOfIntersect GetTypeOfIntersect(Line a, Line b)
@@ -208,6 +222,16 @@ TypeOfIntersect GetTypeOfIntersect(Line a, Line b)
     if (Equal(a.a / b.a, a.b / b.b))
         return EMPTY;
     return POINT;
+}
+
+double Dist(Line a, Line b)
+{
+    if (GetTypeOfIntersect(a, b) != EMPTY)
+        return 0;
+    else
+    {
+        return abs(Norm(a).c - Norm(b).c);
+    }
 }
 
 double serv_Det(double a, double b, double c, double d)
@@ -229,13 +253,104 @@ Point Intersect(Line a, Line b)
     return Point(-serv_Det(a.c, a.b, b.c, b.b) / serv_Det(a.a, a.b, b.a, b.b), -serv_Det(a.a, a.c, b.a, b.c) /  serv_Det(a.a, a.b, b.a, b.b));
 }
 
+struct Ray
+{
+    Point a, b;
+    Ray()
+    {
+        a = Point(0, 0);
+        b = Point(0, 1);
+    }
+    Ray(Point _a, Point _b)
+    {
+        if (_a == _b)
+            GeomLibTechOut += "Runtime Eror: try to get ray by two eqal points in constructor: Ray(Point, Point)\n";
+        a = _a;
+        b = _b;
+    }
+};
+
+Line GetLine(Ray a)
+{
+    return Line(a.a, a.b);
+}
+
+bool OnRay(Ray a, Point b)
+{
+    return ((a.b - a.a) / Len(a.b - a.a) == (b - a.a) / Len(b - a.a));
+}
+
+double Dist(Ray a, Point b)
+{
+    if (OnRay(a, b))
+    {
+        return Dist(GetLine(a), b);
+    }
+    else
+    {
+        return Dist(a.a, b);
+    }
+}
+
+double Dist(Point a, Ray b)
+{
+    return Dist(b, a);
+}
+
+struct Segment
+{
+    Point a, b;
+    Segment()
+    {
+        a = Point(0, 0);
+        b = Point(0, 1);
+    }
+    Segment(Point _a, Point _b)
+    {
+        if (_a == _b)
+        {
+            GeomLibTechOut += "Runtime Eror: try to get segment by two eqal points in constructor: Segment(Point, Point)\n";
+        }
+        a = _a;
+        b = _b;
+    }
+};
+
+istream& operator >> (istream& _in, Segment& a)
+{
+    _in >> a.b >> a.b;
+    return _in;
+}
+
+ostream& operator << (ostream& _out, Segment a)
+{
+    _out << a.a << " " << a.b;
+    return _out;
+}
+
+Line GetLine(Segment a)
+{
+    return Line(a.a, a.b);
+}
+
+bool OnSegment(Segment a, Point b)
+{
+    return OnRay(Ray(a.a, a.b), b) and OnRay(Ray(a.b, a.a), b);
+}
+
+double Dist(Segment a, Point b)
+{
+    return max(Dist(Ray(a.a, a.b), b), Dist(Ray(a.b, a.a), b));
+}
+
+double Dist(Point a, Segment b)
+{
+    return Dist(b, a);
+}
 
 int main()
 {
-    Point a, b, c;
-    cin >> a >> b >> c; 
-    Line f(a, b), s(a, c);
-    Point ans = Intersect(f, s);
-    cout << (ans == a) << endl;
-    cout << GeomLibTechOut;
+    Vector a, b;
+    cin >> a;
+    cout << (a ^ (-1 * a)) << " " << (a ^ (2 * a)) << endl;
 }
